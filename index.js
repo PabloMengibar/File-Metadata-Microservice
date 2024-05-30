d3f9ee2c0b5b6ffe5918702be15a1cf67e4d8ada
Pablo Mengibar <pabmengibar@gmail.com>
2024-05-30 19:57:01 +0000

    fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cors = require('cors');
-var multer  = require('multer'); // Middleware for handling multipart/form-data, used for file uploads
+const fs = require('fs');
+var multer = require('multer'); // Middleware for handling multipart/form-data, used for file uploads
 require('dotenv').config();
 
 var app = express();
@@ -8,30 +9,32 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+const uploadDir = 'uploads';
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 // Set up multer to handle file uploads
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/') // Uploads will be stored in the 'uploads' directory
+    cb(null, 'uploads/'); // Uploads will be stored in the 'uploads' directory
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname) // File name will remain the same as the original name
-  }
+    cb(null, file.originalname); // File name will remain the same as the original name
+  },
 });
 var upload = multer({ storage: storage });
 
 // Route for handling file uploads
-app.post('/upload', upload.single('upfile'), function (req, res) {
-  // req.file contains information about the uploaded file
-  if (!req.file) {
-    return res.status(400).json({ error: 'No file uploaded' });
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  try {
+    res.json({
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size
+    });
+  } catch (err) {
+    res.send(400);
   }
-  
-  // Send back information about the uploaded file
-  res.json({
-    filename: req.file.originalname,
-    type: req.file.mimetype,
-    size: req.file.size
-  });
 });
 
 app.get('/', function (req, res) {
